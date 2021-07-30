f9b494b95503dcfcfdbbe8164ece3c8f36ec4427
leonardo salas <leosalass@gmail.com>
2021-07-30 13:20:47 -0500

    hotfix: fixed method names

--- Assets/_Game/_Client/Scripts/_NetworkLibs/Transport/Systems/TransportClientSystem.cs
+++ Assets/_Game/_Client/Scripts/_NetworkLibs/Transport/Systems/TransportClientSystem.cs
@@ -9,166 +9,169 @@ using Unity.Collections;
 using Unity.Networking.Transport;
 
 
-public class TransportClientSystem : SystemBase
+namespace Client
 {
-    public NetworkDriver driver;
-    public NetworkConnection server;
-    public byte done;
-
-    public JobHandle ClientJobHandle;
-
-    private NativeList<float> lastKeepAlive;
-    private int keepAliveDelay;
-
-
-    protected override void OnCreate()
+    public class TransportClientSystem : SystemBase
     {
-        driver = NetworkDriver.Create();
-        server = default(NetworkConnection);
+        public NetworkDriver driver;
+        public NetworkConnection server;
+        public byte done;
 
-        string serverIp = "127.0.0.1";
-        ushort serverPort = 5522;
-        NetworkEndPoint endPoint = NetworkEndPoint.Parse(serverIp, serverPort);
+        public JobHandle ClientJobHandle;
 
-        keepAliveDelay = 5;
-        lastKeepAlive = new NativeList<float>(1, Allocator.Persistent);
+        private NativeList<float> lastKeepAlive;
+        private int keepAliveDelay;
 
-        server = driver.Connect(endPoint);
 
-        Debug.Log("IsCreated: " + server.IsCreated);
-        if (driver.IsCreated)
+        protected override void OnCreate()
         {
-            lastKeepAlive.Add(0);
-        }
-    }
+            driver = NetworkDriver.Create();
+            server = default(NetworkConnection);
 
-    protected override void OnDestroy()
-    {
-        Debug.LogError("disconected from the server");
-        ClientJobHandle.Complete();
-        driver.Dispose();
-        lastKeepAlive.Dispose();
-    }
+            string serverIp = "127.0.0.1";
+            ushort serverPort = 5522;
+            NetworkEndPoint endPoint = NetworkEndPoint.Parse(serverIp, serverPort);
 
-    protected override void OnUpdate()
-    {
-        ClientJobHandle.Complete();
+            keepAliveDelay = 5;
+            lastKeepAlive = new NativeList<float>(1, Allocator.Persistent);
 
-        var clientUpdateMessagePumpJob = new ClientUpdateMessagePumpJob
-        {
-            driver = driver,
-            server = server,
-            done = done
-        };
+            server = driver.Connect(endPoint);
 
-        var keepAliveJob = new KeepAliveJob
-        {
-            driver = driver,
-            server = server,
-            lastKeepAlive = lastKeepAlive,
-            keepAliveDelay = keepAliveDelay,
-            currentTime = (float)Time.ElapsedTime
-        };
-
-        ClientJobHandle = driver.ScheduleUpdate();
-        ClientJobHandle = clientUpdateMessagePumpJob.Schedule(ClientJobHandle);
-        ClientJobHandle = keepAliveJob.Schedule(ClientJobHandle);
-    }
-}
-
-struct ClientUpdateMessagePumpJob : IJob
-{
-    public NetworkDriver driver;
-    public NetworkConnection server;
-    public byte done;
-
-    public void Execute()
-    {
-        if (!server.IsCreated)
-        {
-            if (done != 1)
-                Debug.Log("Something went wrong during connect, IsCreated: " + server.IsCreated + " done: " + done);
-            return;
-        }
-
-        DataStreamReader stream;
-        NetworkEvent.Type cmd;
-
-        while ((cmd = server.PopEvent(driver, out stream)) != NetworkEvent.Type.Empty)
-        {
-            if (cmd == NetworkEvent.Type.Connect)
+            Debug.Log("IsCreated: " + server.IsCreated);
+            if (driver.IsCreated)
             {
-                connected();
+                lastKeepAlive.Add(0);
             }
-            else if (cmd == NetworkEvent.Type.Data)
-            {
-                NetworkMessage(ref stream);
-            }
-            else if (cmd == NetworkEvent.Type.Disconnect)
-            {
-                disconnected();
-            }
-        }        
-    }
-
-    void connected()
-    {
-        Debug.Log("We are now connected to the server");
-    }
-
-    void disconnected()
-    {
-        Debug.Log("Client got disconnected from server");
-        server = default(NetworkConnection);
-    }
-
-    void NetworkMessage(ref DataStreamReader stream)
-    {
-        byte networkMessageCode = stream.ReadByte();
-        Debug.Log("Got the value = " + networkMessageCode + " back  from the server");
-
-        switch (networkMessageCode)
-        {
-            case (byte)NetworkMessageCode.KeepAlive:
-                Debug.Log("SERVER IS ALIVE");
-                break;
-        }
-    }
-}
-
-
-struct KeepAliveJob : IJob
-{
-    public NetworkDriver driver;
-    public NetworkConnection server;
-    public NativeList<float> lastKeepAlive;
-    public int keepAliveDelay;
-    public float currentTime;
-    public byte done;
-
-    public void Execute()
-    {
-        if (!server.IsCreated)
-        {
-            if (done != 1)
-                Debug.Log("Something went wrong during connect, IsCreated: " + server.IsCreated + " done: " + done);
-            return;
         }
 
-        keepAlive();
+        protected override void OnDestroy()
+        {
+            Debug.LogError("disconected from the server");
+            ClientJobHandle.Complete();
+            driver.Dispose();
+            lastKeepAlive.Dispose();
+        }
+
+        protected override void OnUpdate()
+        {
+            ClientJobHandle.Complete();
+
+            var updateMessagePumpJob = new UpdateMessagePumpJob
+            {
+                driver = driver,
+                server = server,
+                done = done
+            };
+
+            var keepAliveJob = new KeepAliveJob
+            {
+                driver = driver,
+                server = server,
+                lastKeepAlive = lastKeepAlive,
+                keepAliveDelay = keepAliveDelay,
+                currentTime = (float)Time.ElapsedTime
+            };
+
+            ClientJobHandle = driver.ScheduleUpdate();
+            ClientJobHandle = updateMessagePumpJob.Schedule(ClientJobHandle);
+            ClientJobHandle = keepAliveJob.Schedule(ClientJobHandle);
+        }
     }
 
-    void keepAlive()
+    struct UpdateMessagePumpJob : IJob
     {
-        if (lastKeepAlive[0] + keepAliveDelay <= currentTime)
-        {
-            lastKeepAlive[0] = currentTime;
-            Debug.Log(currentTime);
+        public NetworkDriver driver;
+        public NetworkConnection server;
+        public byte done;
 
-            DataStreamWriter writer;
-            driver.BeginSend(server, out writer);
-            writer.WriteByte((byte)NetworkMessageCode.KeepAlive);
-            driver.EndSend(writer);
+        public void Execute()
+        {
+            if (!server.IsCreated)
+            {
+                if (done != 1)
+                    Debug.Log("Something went wrong during connect, IsCreated: " + server.IsCreated + " done: " + done);
+                return;
+            }
+
+            DataStreamReader stream;
+            NetworkEvent.Type cmd;
+
+            while ((cmd = server.PopEvent(driver, out stream)) != NetworkEvent.Type.Empty)
+            {
+                if (cmd == NetworkEvent.Type.Connect)
+                {
+                    connected();
+                }
+                else if (cmd == NetworkEvent.Type.Data)
+                {
+                    NetworkMessages(ref stream);
+                }
+                else if (cmd == NetworkEvent.Type.Disconnect)
+                {
+                    disconnected();
+                }
+            }
+        }
+
+        void connected()
+        {
+            Debug.Log("We are now connected to the server");
+        }
+
+        void disconnected()
+        {
+            Debug.Log("Client got disconnected from server");
+            server = default(NetworkConnection);
+        }
+
+        void NetworkMessages(ref DataStreamReader stream)
+        {
+            byte networkMessageCode = stream.ReadByte();
+            Debug.Log("Got the value = " + networkMessageCode + " back  from the server");
+
+            switch (networkMessageCode)
+            {
+                case (byte)NetworkMessageCode.KeepAlive:
+                    Debug.Log("SERVER IS ALIVE");
+                    break;
+            }
+        }
+    }
+
+
+    struct KeepAliveJob : IJob
+    {
+        public NetworkDriver driver;
+        public NetworkConnection server;
+        public NativeList<float> lastKeepAlive;
+        public int keepAliveDelay;
+        public float currentTime;
+        public byte done;
+
+        public void Execute()
+        {
+            if (!server.IsCreated)
+            {
+                if (done != 1)
+                    Debug.Log("Something went wrong during connect, IsCreated: " + server.IsCreated + " done: " + done);
+                return;
+            }
+
+            keepAlive();
+        }
+
+        void keepAlive()
+        {
+            if (lastKeepAlive[0] + keepAliveDelay <= currentTime)
+            {
+                lastKeepAlive[0] = currentTime;
+                Debug.Log(currentTime);
+
+                DataStreamWriter writer;
+                driver.BeginSend(server, out writer);
+                writer.WriteByte((byte)NetworkMessageCode.KeepAlive);
+                driver.EndSend(writer);
+            }
         }
     }
 }
--- Assets/_Game/_Server/Scripts/_NetworkLibs/Transport/Systems/TransportServerSystem.cs
+++ Assets/_Game/_Server/Scripts/_NetworkLibs/Transport/Systems/TransportServerSystem.cs
@@ -8,186 +8,190 @@ using Unity.Networking.Transport;
 using System.IO;
 using System.Reflection;
 
-[UpdateInGroup(typeof(SimulationSystemGroup))]
-public class TransportServerSystem : SystemBase
+namespace Server
 {
-    public NetworkDriver driver;
-    public NativeList<NetworkConnection> connections;
-    private NativeList<float> lastKeepAlives;
-    private int keepAliveDelay;
 
-    private JobHandle ServerJobHandle;
-
-    protected override void OnCreate()
+    [UpdateInGroup(typeof(SimulationSystemGroup))]
+    public class TransportServerSystem : SystemBase
     {
-        driver = NetworkDriver.Create();
-        NetworkEndPoint endPoint = NetworkEndPoint.AnyIpv4;
-        endPoint.Port = 5522;
+        public NetworkDriver driver;
+        public NativeList<NetworkConnection> connections;
+        private NativeList<float> lastKeepAlives;
+        private int keepAliveDelay;
 
-        if (IsPortAvailable(driver, endPoint))
+        private JobHandle ServerJobHandle;
+
+        protected override void OnCreate()
         {
-            driver.Listen();
-            if (driver.Listening)
+            driver = NetworkDriver.Create();
+            NetworkEndPoint endPoint = NetworkEndPoint.AnyIpv4;
+            endPoint.Port = 5522;
+
+            if (IsPortAvailable(driver, endPoint))
             {
-                Debug.Log("Listening for connections");
+                driver.Listen();
+                if (driver.Listening)
+                {
+                    Debug.Log("Listening for connections");
+                }
+            }
+
+            keepAliveDelay = 5;
+            int maxConnections = 10;
+
+            connections = new NativeList<NetworkConnection>(maxConnections, Allocator.Persistent);
+            lastKeepAlives = new NativeList<float>(maxConnections, Allocator.Persistent);
+        }
+
+        protected override void OnDestroy()
+        {
+            // Make sure we run our jobs to completion before exiting.
+            ServerJobHandle.Complete();
+            driver.Dispose();
+            connections.Dispose();
+            lastKeepAlives.Dispose();
+        }
+
+        protected override void OnUpdate()
+        {
+            ServerJobHandle.Complete();
+
+            float currentTime = (float)Time.ElapsedTime;
+
+            var updateConnectionsJob = new UpdateConnectionsJob
+            {
+                driver = driver,
+                connections = connections,
+                lastKeepAlives = lastKeepAlives,
+                currentTime = currentTime
+            };
+
+            var updateMessagePumpJump = new UpdateMessagePumpJob
+            {
+                driver = driver.ToConcurrent(),
+                connections = connections.AsDeferredJobArray(),
+                lastKeepAlives = lastKeepAlives.AsDeferredJobArray(),
+                keepAliveDelay = keepAliveDelay,
+                currentTime = currentTime
+            };
+
+            ServerJobHandle = driver.ScheduleUpdate();
+            ServerJobHandle = updateConnectionsJob.Schedule(ServerJobHandle);
+            ServerJobHandle = updateMessagePumpJump.Schedule(connections, 1, ServerJobHandle);
+        }
+
+        private bool IsPortAvailable(NetworkDriver driver, NetworkEndPoint endPoint)
+        {
+            bool isOpen = (driver.Bind(endPoint) != 0) ? false : true;
+
+            if (!isOpen)
+            {
+                Debug.Log("There was an error binding to port " + endPoint.Port);
+            }
+
+            return isOpen;
+        }
+    }
+
+    struct UpdateConnectionsJob : IJob
+    {
+        public NetworkDriver driver;
+        public NativeList<NetworkConnection> connections;
+        public NativeList<float> lastKeepAlives;
+        public float currentTime;
+
+        public void Execute()
+        {
+            CleanUpConnections();
+            AcceptNewConnections();
+        }
+
+        void CleanUpConnections()
+        {
+            for (int i = 0; i < connections.Length; i++)
+            {
+                if (!connections[i].IsCreated)
+                {
+                    connections.RemoveAtSwapBack(i);
+                    lastKeepAlives.RemoveAtSwapBack(i);
+                    --i;
+                }
             }
         }
 
-        keepAliveDelay = 5;
-        int maxConnections = 10;
-
-        connections = new NativeList<NetworkConnection>(maxConnections, Allocator.Persistent);
-        lastKeepAlives = new NativeList<float>(maxConnections, Allocator.Persistent);
-    }
-
-    protected override void OnDestroy()
-    {
-        // Make sure we run our jobs to completion before exiting.
-        ServerJobHandle.Complete();
-        driver.Dispose();
-        connections.Dispose();
-        lastKeepAlives.Dispose();
-    }
-
-    protected override void OnUpdate()
-    {
-        ServerJobHandle.Complete();
-
-        float currentTime = (float)Time.ElapsedTime;
-
-        var serverUpdateMessagePumpJob = new ServerUpdateMessagePumpJob
+        void AcceptNewConnections()
         {
-            driver = driver,
-            connections = connections,
-            lastKeepAlives = lastKeepAlives,
-            currentTime = currentTime
-        };
-
-        var serverKeepAliveJob = new ServerKeepAliveJob
-        {
-            driver = driver.ToConcurrent(),
-            connections = connections.AsDeferredJobArray(),
-            lastKeepAlives = lastKeepAlives.AsDeferredJobArray(),
-            keepAliveDelay = keepAliveDelay,
-            currentTime = currentTime
-        };
-
-        ServerJobHandle = driver.ScheduleUpdate();
-        ServerJobHandle = serverUpdateMessagePumpJob.Schedule(ServerJobHandle);
-        ServerJobHandle = serverKeepAliveJob.Schedule(connections, 1, ServerJobHandle);
-    }
-
-    private bool IsPortAvailable(NetworkDriver driver, NetworkEndPoint endPoint)
-    {
-        bool isOpen = (driver.Bind(endPoint) != 0) ? false : true;
-
-        if (!isOpen)
-        {
-            Debug.Log("There was an error binding to port " + endPoint.Port);
-        }
-
-        return isOpen;
-    }
-}
-
-struct ServerUpdateMessagePumpJob : IJob
-{
-    public NetworkDriver driver;
-    public NativeList<NetworkConnection> connections;
-    public NativeList<float> lastKeepAlives;
-    public float currentTime;
-
-    public void Execute()
-    {
-        CleanUpConnections();
-        AcceptNewConnections();
-    }
-
-    void CleanUpConnections()
-    {
-        for (int i = 0; i < connections.Length; i++)
-        {
-            if (!connections[i].IsCreated)
+            NetworkConnection c;
+            while ((c = driver.Accept()) != default(NetworkConnection))
             {
-                connections.RemoveAtSwapBack(i);
-                lastKeepAlives.RemoveAtSwapBack(i);
-                --i;
+                connections.Add(c);
+                lastKeepAlives.Add(currentTime);
+                Debug.Log("Accepted a connection");
             }
         }
     }
 
-    void AcceptNewConnections()
+    struct UpdateMessagePumpJob : IJobParallelForDefer
     {
-        NetworkConnection c;
-        while ((c = driver.Accept()) != default(NetworkConnection))
+        // Start querying the driver for events
+        // that might have happened since the last update(tick).
+
+        public NetworkDriver.Concurrent driver;
+        public NativeArray<NetworkConnection> connections;
+        public NativeArray<float> lastKeepAlives;
+        public int keepAliveDelay;
+        public float currentTime;
+
+        public void Execute(int index)
         {
-            connections.Add(c);
-            lastKeepAlives.Add(currentTime);
-            Debug.Log("Accepted a connection");
-        }
-    }
-}
+            //Begin by defining a DataStreamReader.
+            //This will be used in case any Data event was received.
+            //Then we just start looping through all our connections.
+            DataStreamReader stream;
+            NetworkEvent.Type cmd;
 
-struct ServerKeepAliveJob : IJobParallelForDefer
-{
-    // Start querying the driver for events
-    // that might have happened since the last update(tick).
-
-    public NetworkDriver.Concurrent driver;
-    public NativeArray<NetworkConnection> connections;
-    public NativeArray<float> lastKeepAlives;
-    public int keepAliveDelay;
-    public float currentTime;
-
-    public void Execute(int index)
-    {
-        //Begin by defining a DataStreamReader.
-        //This will be used in case any Data event was received.
-        //Then we just start looping through all our connections.
-        DataStreamReader stream;
-        NetworkEvent.Type cmd;
-
-        Assert.IsTrue(connections[index].IsCreated);
-        while ((cmd = driver.PopEventForConnection(connections[index], out stream)) != NetworkEvent.Type.Empty)
-        {
-            if (cmd == NetworkEvent.Type.Data)
+            Assert.IsTrue(connections[index].IsCreated);
+            while ((cmd = driver.PopEventForConnection(connections[index], out stream)) != NetworkEvent.Type.Empty)
             {
-                Receive(stream);
+                if (cmd == NetworkEvent.Type.Data)
+                {
+                    NetworkMessages(stream);
+                }
+                else if (cmd == NetworkEvent.Type.Disconnect)
+                {
+                    Disconnect(index);
+                }
             }
-            else if (cmd == NetworkEvent.Type.Disconnect)
-            {
-                Disconnect(index);
-            }
+
+            KeepAlive();
         }
 
-        KeepAlive();
-    }
-
-    void Receive(DataStreamReader stream)
-    {
-        byte messageCode = stream.ReadByte();
-        Debug.Log("Got " + messageCode + " as message code.");
-    }
-
-    void Disconnect(int index)
-    {
-        Debug.Log("Client disconnected from server");
-        connections[index] = default(NetworkConnection);
-        lastKeepAlives[index] = 0;
-    }
-
-    void KeepAlive()
-    {
-        for (int i = 0; i < connections.Length; i++)
+        void NetworkMessages(DataStreamReader stream)
         {
-            if (lastKeepAlives[i] + keepAliveDelay <= currentTime)
-            {
-                DataStreamWriter writer;
-                driver.BeginSend(connections[i], out writer);
-                writer.WriteInt(1);
-                driver.EndSend(writer);
+            byte messageCode = stream.ReadByte();
+            Debug.Log("Got " + messageCode + " as message code.");
+        }
 
-                lastKeepAlives[i] = currentTime;
+        void Disconnect(int index)
+        {
+            Debug.Log("Client disconnected from server");
+            connections[index] = default(NetworkConnection);
+            lastKeepAlives[index] = 0;
+        }
+
+        void KeepAlive()
+        {
+            for (int i = 0; i < connections.Length; i++)
+            {
+                if (lastKeepAlives[i] + keepAliveDelay <= currentTime)
+                {
+                    DataStreamWriter writer;
+                    driver.BeginSend(connections[i], out writer);
+                    writer.WriteInt(1);
+                    driver.EndSend(writer);
+
+                    lastKeepAlives[i] = currentTime;
+                }
             }
         }
     }
