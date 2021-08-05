a12e3ddfa0ff9967b446452313ac9522b1ef77c6
Omar Plata Salas <omarps464@gmail.com>
2021-08-05 16:05:53 -0500

    remove unused entity spawner reference

--- Assets/_Game/_Client/Scripts/_NetworkLibs/Transport/Systems/TransportClientSystem.cs
+++ Assets/_Game/_Client/Scripts/_NetworkLibs/Transport/Systems/TransportClientSystem.cs
@@ -27,8 +27,6 @@ namespace Client
         private float currentTime;
         private bool waitingKeepAliveResponse;
 
-        EntitySpawner entitySpawner;
-
         protected override void OnCreate()
         {
             driver = NetworkDriver.Create();
@@ -47,7 +45,6 @@ namespace Client
             if (driver.IsCreated)
             {
                 Debug.Log("Connecting to server...");
-                entitySpawner = new EntitySpawner();
             }
         }
 
@@ -75,7 +72,6 @@ namespace Client
                 if (cmd == NetworkEvent.Type.Connect)
                 {
                     Debug.Log("We are now connected to the server");
-                    entitySpawner.CreateServerEntity();
                 }
                 else if (cmd == NetworkEvent.Type.Data)
                 {
